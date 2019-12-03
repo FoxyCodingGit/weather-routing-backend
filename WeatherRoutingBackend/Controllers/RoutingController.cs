e6fd6abbf4945af6966513363793060a7fed9a48
Adam Fox <Adam.Fox@derivco.co.uk>
2019-12-03 08:58:36 +0000

    change backend to get multiple routes.

--- WeatherRoutingBackend/Controllers/RoutingController.cs
+++ WeatherRoutingBackend/Controllers/RoutingController.cs
@@ -15,28 +15,36 @@ namespace WeatherRoutingBackend.Controllers
         private static readonly HttpClient Client = new HttpClient(); // needs to only be init once, so need to move out into own base service.
 
         [HttpGet]
-        [Route("{travelMode}/{startLat}/{startLng}/{endLat}/{endLng}")]
-        public async Task<PointsTimeAndDistance> GetRouteLegsAndTime(string travelMode, double startLat, double startLng, double endLat, double endLng)
+        [Route("{travelMode}/{numberOfAlternates}/{startLat}/{startLng}/{endLat}/{endLng}")]
+        public async Task<PointsTimeAndDistance[]> GetRouteLegsAndTime(string travelMode, int numberOfAlternates, double startLat, double startLng, double endLat, double endLng)
         {
             var key = "dzTX2x3ocGZPLzhVGol51CtFKBX7hD63";
 
             HttpResponseMessage response =
                 await Client.GetAsync(
-                    $"https://api.tomtom.com/routing/1/calculateRoute/{startLat},{startLng}:{endLat},{endLng}/json?travelMode={travelMode}&key={key}");
+                    $"https://api.tomtom.com/routing/1/calculateRoute/{startLat},{startLng}:{endLat},{endLng}/json?travelMode={travelMode}&maxAlternatives={numberOfAlternates}&key={key}");
 
 
             var jsonString = await response.Content.ReadAsStringAsync(); // need to catch errors here as if get so. Then just 500 is added by code below.
-            var adam =  JsonConvert.DeserializeObject<RouteResponse>(jsonString);
+            var adam = JsonConvert.DeserializeObject<RouteResponse>(jsonString);
 
-            var justPoints = adam.Routes.ToList()[0].Legs.ToList()[0].Points;
+            PointsTimeAndDistance[] routes = new PointsTimeAndDistance[adam.Routes.ToArray().Length];
 
-
-            return new PointsTimeAndDistance
+            for (int i = 0; i < adam.Routes.ToArray().Length; i++)
             {
-                Points = justPoints,
-                TravelTimeInSeconds = adam.Routes.ToList()[0].Summary.TravelTimeInSeconds,
-                Distance = adam.Routes.ToList()[0].Summary.LengthInMeters
-            };
+                var justPoints = adam.Routes.ToList()[i].Legs.ToList()[0].Points; // DONT KNOW HOW MAXALT WORKS. IS IT A ROUTE OR A LEG????
+
+                routes[i] = new PointsTimeAndDistance
+                {
+                    Points = justPoints,
+                    TravelTimeInSeconds = adam.Routes.ToList()[i].Summary.TravelTimeInSeconds,
+                    Distance = adam.Routes.ToList()[i].Summary.LengthInMeters
+                };
+            }
+
+            return routes;
+
+
         }
     }
 }
