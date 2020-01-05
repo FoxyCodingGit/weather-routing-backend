5b20f6e72016fcb56963eaf7f969553f28a3ed03
Adam <af17923@essex.ac.uk>
2020-01-05 20:27:29 +0000

    add call for current weather information

--- WeatherRoutingBackend/Controllers/WeatherController.cs
+++ WeatherRoutingBackend/Controllers/WeatherController.cs
@@ -69,5 +69,24 @@ namespace WeatherRoutingBackend.Controllers
 
             return JsonConvert.DeserializeObject<WeatherResponse>(jsonString).Minutely.Data;
         }
+
+        [HttpGet]
+        [Route("rain/currently/{lat}/{lng}")]
+        public async Task<Currently> GetCurrentWeatherForPoint(double lat, double lng)
+        {
+            const string key = "c2cae150bc54b0e5884a2e74a974152a";
+
+            HttpResponseMessage response =
+                await Client.GetAsync(
+                    $"https://api.darksky.net/forecast/{key}/{lat},{lng}?units=si"); // add unit in request.
+
+            var jsonString =
+                await response.Content
+                    .ReadAsStringAsync(); // need to catch errors here as if get so. Then just 500 is added by code below.
+
+            return JsonConvert.DeserializeObject<WeatherResponse>(jsonString).Currently;
+        }
+
+
     }
 }
