bb2900bdc131d808e787b7693d2571eeeb625fae
Adam Fox <Adam.Fox@derivco.co.uk>
2020-01-29 15:48:18 +0000

    change to better name

--- WeatherRoutingBackend/DataLayer/Models/ReadableRoute.cs
+++ WeatherRoutingBackend/DataLayer/Models/ReadableRoute.cs
@@ -4,7 +4,7 @@
     {
         public int ReadableRouteId { get; set; }
         public string RouteName { get; set; }
-        public string ModeOfTransportName { get; set; }
+        public string ModeOfTransport { get; set; }
         public decimal StartLat { get; set; }
         public decimal StartLng { get; set; }
         public decimal EndLat { get; set; }
